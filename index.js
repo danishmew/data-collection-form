3333050c51a784c25ea352f9ba069283e82f216e
danishmew <danishmew@gmail.com>
2024-03-24 02:13:29 +0500

    first-commit

---
+++ index.js
@@ -0,0 +1,14 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const questions = [
+    { message: "Please enter first name: ", type: "input", name: "firstName" },
+    { message: "Please enter last name: ", type: "input", name: "lasttName" },
+    { message: "Please enter father name: ", type: "input", name: "fatherName" },
+    { message: "Please enter email address: ", type: "input", name: "emailAddress" },
+    { message: "Please enter last qualification: ", type: "input", name: "lastQualification" },
+    { message: "Select gender: ", type: "input", name: "genderName" },
+];
+const answer = await inquirer.prompt(questions);
+console.log("\n Thanks for your precious time.");
+console.log(answer);
+console.log("\nAbove mentioned data collect just used for education perpose only.");
